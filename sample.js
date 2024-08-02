ef5b759f31565a0fc31609992d3cd16453acc62c
“pavankumarpalaka” <sivagovindharao@gmail.com>
2024-08-02 19:24:42 +0530

    aug1sample

---
+++ sample.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("pkp")[0].textContent="pavan kumar"
+
+document.getElementById("pk").innerHTML="<p> from Anakapalle</p>"
+
+const con = document.createElement('names');
+con.innerHTML="srkr"
+document.body.appendChild(con);
